3451ea0a8bdda00dcdb3743d66cbb1f69f551961
LuuThom <thom0702>
2019-04-16 17:32:11 +0700

    sideBar

--- source/js/_custom.js
+++ source/js/_custom.js
@@ -1,5 +1,3 @@
-import PerfectScrollbar from "perfect-scrollbar";
-
 $(function () {
   $('.datetimepicker').datetimepicker();
   //slider
@@ -16,16 +14,35 @@ $(function () {
     })
 
   });
-  // Js crollbar notifi
-  var scroll = document.querySelector('.js-list-notifi');
-  var ps = new PerfectScrollbar(scroll);
-  const ps = new PerfectScrollbar('.js-list-notifi', {
-    wheelPropagation: false,
-  });
+  // Js scrollbar notifi
+  // var listScroll = document.querySelectorAll(".js-list, .js-chat-section");
+  // for (var i = 0; i < listScroll.length; i++) {
+  //   new PerfectScrollbar(listScroll[i], {
+  //     wheelPropagation: false
+  //   });
+  // }
+  var ps = new PerfectScrollbar('.js-list');
 
+  // Js scrollbar chat
+  // var pt = new PerfectScrollbar('.js-chat-section');
+
+  // show sidebarRight
+
+  $(document).on('mouseup', function (e) {
+    if (document.getElementsByClassName('js-settings')[0].contains(e.target)) {
+      $('.js-sidebar-show').toggleClass('sidebar-show');
+
+    }
+    else {
+      var container = $('.js-sidebar-show');
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        $('.js-sidebar-show').removeClass('sidebar-show');
+      }
+    }
+    });
   // sparkline
-  $('#spark1').sparkline([0,5,3,7,5,10,3,6,10], { 
-    type:'line',
+  $('#spark1').sparkline([0, 5, 3, 7, 5, 10, 3, 6, 10], {
+    type: 'line',
     pointDot: false,
     spotColor: false,
     minSpotColor: false,
@@ -36,8 +53,8 @@ $(function () {
     width: '85',
     height: '35',
   });
-  $('#spark2').sparkline([5,8,7,10,9,10,8,6,4,6,8,7,6,8], { 
-    type:'bar', barColor:'#fbbc05',
+  $('#spark2').sparkline([5, 8, 7, 10, 9, 10, 8, 6, 4, 6, 8, 7, 6, 8], {
+    type: 'bar', barColor: '#fbbc05',
     pointDot: false,
     spotColor: false,
     minSpotColor: false,
@@ -47,8 +64,8 @@ $(function () {
     width: '81',
     height: '35',
   });
-  $('#spark3').sparkline([2,3,4,5,4,3,2,3,4,5,4,3,2,3,4,5,4,3,2], { 
-    type:'discrete', lineColor: '#007f00',
+  $('#spark3').sparkline([2, 3, 4, 5, 4, 3, 2, 3, 4, 5, 4, 3, 2, 3, 4, 5, 4, 3, 2], {
+    type: 'discrete', lineColor: '#007f00',
     pointDot: false,
     spotColor: false,
     minSpotColor: false,
@@ -58,8 +75,8 @@ $(function () {
     width: '85',
     height: '35',
   });
-  $('#spark4').sparkline([0,5,3,7,5,10,3,6,10], { 
-    type:'line',
+  $('#spark4').sparkline([0, 5, 3, 7, 5, 10, 3, 6, 10], {
+    type: 'line',
     lineColor: 'red',
     pointDot: false,
     spotColor: false,
@@ -71,24 +88,24 @@ $(function () {
     height: '35',
     lineHeight: '10',
   });
-  
+
   // countup
-  $('#number').counterUp({
-    delay: 10,
-    time: 1000
-  });
+  // $('#number').counterUp({
+  //   delay: 10,
+  //   time: 1000
+  // });
 
 
   // custom file
 
   $('input[multiple="multiple"]').each(function (index, element) {
     var $this = $(this);
-    $this.change(function (e) { 
+    $this.change(function (e) {
       e.preventDefault();
       $this.next().find('span').eq(0).html(this.value.split("\\").pop());
     });
   });
-  
+
 
   $(document).on('click', function (event) {
     if (!$(event.target).closest('.dropdown-select').length) {
--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@
 //=require bootstrap/dist/js/bootstrap.min.js
 //=require bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js
 //=require jquery-sparkline/jquery.sparkline.min.js
-//=require perfect-scrollbar/perfect-scrollbar.min.js
+//=require perfect-scrollbar/dist/perfect-scrollbar.min.js
 //=require countup/dist/countUp.min.js
 //=require bootstrap-slider/dist/bootstrap-slider.min.js
 //=require _custom.js
